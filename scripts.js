e9fe67d47c6975b2575e483fe1ce6487a064ba24
acortexot <acortexot@gmail.com>
2022-08-21 20:15:40 -0300

    Add backspace, negative and period functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 let firstNumber = "";
 let secondNumber
 let operator
-
 const screen = document.querySelector('.screen');
-//Input numbers
+//Number button functionality
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number)=> {
     number.addEventListener('click', ()=> {
@@ -11,6 +10,40 @@ numbers.forEach((number)=> {
         firstNumber = firstNumber + number.textContent;
     })
 })
+//Clear button functionality
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', ()=> {
+    screen.textContent = '0';
+    firstNumber = '';
+})
+//Backspace button functionality
+const backspace = document.querySelector('#backspace');
+backspace.addEventListener('click', ()=>{
+    const numberArr = Array.from(screen.textContent)    //create array from input
+    const removedNum = numberArr.pop(); 
+    screen.textContent = numberArr.join('');    //convert new array back into string
+    firstNumber = numberArr.join('');   //update variable with new number  
+})
+//Negative button functionality
+const negative = document.querySelector('#negative');
+negative.addEventListener('click', ()=>{
+    const numberArr = Array.from(screen.textContent)    //create array from input
+    if (numberArr[0] === '-') {
+        const removedSign = numberArr.shift();
+    } else {
+        numberArr.unshift('-');
+    }; 
+    screen.textContent = numberArr.join('');    //convert new array back into string
+    firstNumber = numberArr.join('');   //update variable with new number  
+})
+//Period button functionality
+const period = document.querySelector('#period');
+    period.addEventListener('click', ()=> {
+    const currentNumber = screen.textContent;
+        if (currentNumber.includes('.')) {return}   //interrupt if period is there
+        screen.textContent = firstNumber + '.';
+        firstNumber = firstNumber + '.';
+    })
 //Calculation functions
 function add(a,b) {
     return a+b;
